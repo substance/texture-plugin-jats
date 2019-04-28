cca13cd988bfa829da53e354aa6ad8b1a50b2a55
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-04-28 14:11:57 +0200

    Improve bundling.

--- make.js
+++ make.js
@@ -1,7 +1,7 @@
-/* eslint-disable no-template-curly-in-string */
 const b = require('substance-bundler')
 const path = require('path')
 const compileSchema = require('texture-xml-utils/bundler/compileSchema')
+const generateSchemaDocumentation = require('texture-xml-utils/bundler/generateSchemaDocumentation')
 
 const DIST = 'dist/'
 const TMP = 'tmp/'
@@ -11,24 +11,30 @@ b.task('clean', function () {
   b.rm(TMP)
 })
 
-b.task('publish', ['clean', 'build:schema', 'build:plugin'])
-
 b.task('default', ['publish'])
+
+b.task('publish', ['clean', 'build'])
 
 b.task('build', ['build:schema', 'build:plugin'])
 
 b.task('build:schema', ['build:schema:jats-archiving-1.0', 'build:schema:jats-archiving-1.1', 'build:schema:jats-archiving-1.2'])
 
 b.task('build:schema:jats-archiving-1.0', () => {
-  compileSchema(b, 'JATS-archiving-1.0', path.join(__dirname, 'rng', 'jats-archiving-rng-1.0', 'JATS-archivearticle1.rng'))
+  let rngFile = path.join(__dirname, 'rng', 'jats-archiving-rng-1.0', 'JATS-archivearticle1.rng')
+  let name = 'JATS-archiving-1.0'
+  _buildSchema(name, rngFile)
 })
 
 b.task('build:schema:jats-archiving-1.1', () => {
-  compileSchema(b, 'JATS-archiving-1.1', path.join(__dirname, 'rng', 'JATS-Archiving-1-1-MathML3-DTD-RNG', 'JATS-archivearticle1-mathml3.rng'))
+  let rngFile = path.join(__dirname, 'rng', 'JATS-Archiving-1-1-MathML3-DTD-RNG', 'JATS-archivearticle1-mathml3.rng')
+  let name = 'JATS-archiving-1.1'
+  _buildSchema(name, rngFile)
 })
 
 b.task('build:schema:jats-archiving-1.2', () => {
-  compileSchema(b, 'JATS-archiving-1.2', path.join(__dirname, 'rng', 'JATS-Archiving-1-2-MathML3-RNG', 'JATS-archivearticle1-mathml3.rng'))
+  let rngFile = path.join(__dirname, 'rng', 'JATS-Archiving-1-2-MathML3-RNG', 'JATS-archivearticle1-mathml3.rng')
+  let name = 'JATS-archiving-1.2'
+  _buildSchema(name, rngFile)
 })
 
 b.task('build:plugin', () => {
@@ -46,4 +52,18 @@ b.task('build:plugin', () => {
     ],
     external: [ 'substance', 'texture' ]
   })
+  b.minify(DIST + 'texture-plugin-jats.js', {
+    debug: false
+  })
 })
+
+function _buildSchema (name, rngFile) {
+  compileSchema(b, rngFile, {
+    name,
+    dest: TMP + `${name}.data.js`
+  })
+  generateSchemaDocumentation(b, rngFile, {
+    name,
+    dest: TMP + `${name}.md`
+  })
+}
